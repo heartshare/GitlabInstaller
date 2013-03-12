5d31286cb9d446a979fda962453b3c49611aeef2
heartshare <heartshare@gmail.com>
2013-03-13 00:42:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ sudo ln -s /usr/bin/python /usr/bin/python2
 sudo DEBIAN_FRONTEND='noninteractive' apt-get install -y postfix-policyd-spf-python postfix 
 
 
-http://mirrors.ibiblio.org/ruby/1.9/ruby-1.9.3-p327.tar.gz
+wget http://mirrors.ibiblio.org/ruby/1.9/ruby-1.9.3-p327.tar.gz
 tar xfvz ruby-1.9.3-p327.tar.gz
 cd ruby-1.9.3-p327
 ./configure
